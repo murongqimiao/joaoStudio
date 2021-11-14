accc0e129a5d261b70e4c29d3a611cb3e081c037
joao <murongqimiao@live.cn>
2021-11-14 16:56:38 +0800

    feat: add webpack copy plugin

--- startGame/webpack.config.js
+++ startGame/webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+
 module.exports = {
     mode: 'development',
     entry: path.resolve('./src/'),
@@ -6,5 +9,10 @@ module.exports = {
         path: path.resolve('./dist'),
         filename: 'file.js',
         pathinfo: true
-    }
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [{ from: "./src/index.html", to: "./index.html" },]
+        })
+    ]
 };
