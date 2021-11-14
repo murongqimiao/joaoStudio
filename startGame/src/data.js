f0fe1a8a45f884c56f83d164ccbe78e526ba9c6e
joao <murongqimiao@live.cn>
2021-11-14 20:02:23 +0800

    feat: 行走动画完成

--- startGame/src/data.js
+++ startGame/src/data.js
@@ -1,10 +1,33 @@
-const CONSTANT_COMMON = {
+export const CONSTANT_COMMON = {
     BASE_HERO_HP: 3,
     BASE_HERO_ATK: 1,
     BASE_HERO_DEF: 0,
     BASE_HERO_MAGIC: 3,
     BASE_HERO_SPD: 1,
     BASE_ONE_SECOND: 60,
+    DPR: 3,
+}
+
+const CONSTANT_IMG = {
+    FOOT_MAN_WALKING: 'FOOT_MAN_WALKING', // 图片位置
+    FOOT_MAN_WALKING_SIZE: '1024_1024', // 图片尺寸
+}
+
+export const getImageFromX_Y = (imgClass, X_Y) => {
+    // 1_1_8_8 表示图片是8X8, imgClass找对应图片size, 除以即可 抛出结果, 
+    const imgSize = CONSTANT_IMG[imgClass + '_SIZE']
+    const [imgWidth, imgHeight] = imgSize.split('_')
+    const [colNth, rowNth, rowMax, colMax] = X_Y.split('_')
+    const perRow = imgWidth / rowMax
+    const perCol = imgHeight / colMax
+    return {
+        sx: (rowNth - 1) * perRow,
+        sy: (colNth - 1) * perCol,
+        swidth: perRow,
+        sheight: perCol,
+        width: perRow,
+        height: perCol
+    }
 }
 
 /**
@@ -27,10 +50,43 @@ export const footMan = {
     skill: {
         cd: CONSTANT_COMMON.BASE_ONE_SECOND
     },
+    // 图片渲染的优化方向, 提供一个使用大图的高宽及横纵下标返回图片的能力
+    framePerChange: {
+        TOP: 10,
+        TOP_LEFT: 10,
+        TOP_RIGHT: 10,
+        LEFT: 10,
+        RIGHT: 10,
+        BOTTOM: 10,
+        BOTTOM_LEFT: 10,
+        BOTTOM_RIGHT: 10
+    },
+    framesList: {
+        TOP: [{ imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '3_1_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '3_2_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '3_3_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '3_4_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '3_5_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '3_6_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '3_7_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '3_8_8_8' }],
+        TOP_LEFT: [{ imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '2_1_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '2_2_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '2_3_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '2_4_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '2_5_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '2_6_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '2_7_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '2_8_8_8' }],
+        TOP_RIGHT: [{ imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '4_1_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '4_2_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '4_3_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '4_4_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '4_5_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '4_6_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '4_7_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '4_8_8_8' }],
+        LEFT: [{ imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '1_1_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '1_2_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '1_3_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '1_4_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '1_5_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '1_6_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '1_7_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '1_8_8_8' }],
+        RIGHT: [{ imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '5_1_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '5_2_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '5_3_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '5_4_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '5_5_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '5_6_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '5_7_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '5_8_8_8' }],
+        BOTTOM: [{ imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '7_1_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '7_2_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '7_3_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '7_4_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '7_5_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '7_6_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '7_7_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '7_8_8_8' }],
+        BOTTOM_LEFT: [{ imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '8_1_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '8_2_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '8_3_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '8_4_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '8_5_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '8_6_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '8_7_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '8_8_8_8' }],
+        BOTTOM_RIGHT: [{ imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '6_1_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '6_2_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '6_3_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '6_4_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '6_5_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '6_6_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '6_7_8_8' }, { imgClass: CONSTANT_IMG.FOOT_MAN_WALKING, imgLR: '6_8_8_8' }],
+    },
     onHeroAdd: function () {
         const [game, self] = arguments
         console.log("大敌当前,十分恐惧, hp-1")
         self.state.hp--
         console.log(self)
     }
+}
+
+
+/**
+ * action info {}
+ * 
+ */
+export const walking = function () {
+    // console.log("执行了walking")
+    // console.log(this)
+    this.curEvent = 'RIGHT'
+    return this;
 }
--- startGame/src/index.js
+++ startGame/src/index.js
@@ -1,4 +1,4 @@
-import { footMan } from "./data"
+import { footMan, walking, getImageFromX_Y, CONSTANT_COMMON } from "./data"
 
 class Game {
     monsterList = []
@@ -8,9 +8,20 @@ class Game {
     }
 
     run() {
+        // 执行行为
+        this.monsterList.forEach(v => v.action && v.action())
+        this.heroList.forEach(v => v.action && v.action())
+
+        // 执行render
+        const allRenderList = [].concat(this.monsterList).concat(this.heroList)
+        var c = document.getElementById('canvas');
+        var ctx = c.getContext("2d");
+        ctx.clearRect(0, 0, c.width, c.height)
+        allRenderList.forEach(v => v.render({ ctx }))
+
         // 执行下一帧
         window.requestAnimationFrame(() => {
-            // this.run()
+            this.run()
         })
     }
 
@@ -31,6 +42,7 @@ class Game {
         console.log(hero)
         this.heroList.push(hero)
         hero.onHeroAdd && this.onHeroAdd(hero)
+        return this;
     }
 
     // 钩子
@@ -47,22 +59,74 @@ class Role {
     skill = {}
     position = {}
     onHeroAdd = null
+    curEvent = null // 记录当前执行的事件
+    curRender = {
+        imgClass: null,
+        curFrameImgIndex: 0,
+        curFrame: 0,
+    } // 记录当前渲染内容
+    frameList = {}
+    frameInfo = {
+        frameList: {
+            walking
+        },
+        curFrame: 0,
+        frameCount: 0,
+    }
     constructor(props) {
         this.state = props.role
         this.skill = props.skill
         this.onHeroAdd = props.onHeroAdd || null
         this.onMonsterAdd = props.onMonsterAdd || null
-        this.position = props.position || { x: 100, y: 100, z: 0 }
+        this.framesList = props.framesList || {}
+        this.framePerChange = props.framePerChange || {}
+    }
+    addPosition(params) {
+        const { x, y, z = 0 } = params;
+        this.position = { x, y, z }
+        return this;
+    }
+    // 渲染逻辑 找到指定的某个图片 某一帧  渲染到canvas里
+    render() {
+        const { ctx } = arguments[0]
+        if (this.framesList[this.curEvent]) {
+            // 命中当前行为 进行渲染
+            if (this.curRender.curFrame === this.framePerChange[this.curEvent]) { // 动画行进到下一张
+                if (this.curRender.curFrameImgIndex === this.framesList[this.curEvent].length - 1) { // 重复动画归0
+                    this.curRender.curFrameImgIndex = 0
+                    this.curRender.curFrame = 0
+                } else {
+                    this.curRender.curFrameImgIndex++
+                    this.curRender.curFrame = 0
+                }
+            }
+            const { imgClass, imgLR } = this.framesList[this.curEvent][this.curRender.curFrameImgIndex]
+            const { sx, sy, swidth, sheight, width, height, } = getImageFromX_Y(imgClass, imgLR)
+            const Img = document.getElementsByClassName(imgClass)[0]
+            const { x, y } = this.position
+            ctx.drawImage(Img, sx, sy, swidth, sheight, x, y, width, height)
+            this.curRender.curFrame++
+        }
+    }
+    addAction(eventName, func) {
+        this[eventName] = func.bind(this)
+        return this;
     }
 }
 
 const footManNew = new Role(footMan)
 const gameNew = new Game()
 
+footManNew.addPosition({ x: 0, y: 0, z: 0 }).addAction('action', walking)
+
 gameNew.addNewHero(footManNew)
 
 
-gameNew.start()
+
+
+setTimeout(() => {
+    gameNew.start()
+}, 1000)
 
 
 
