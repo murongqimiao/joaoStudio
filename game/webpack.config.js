2fb0229db26bf21b1ef878085a6b12bfe1a97f73
murongqimiao <murongqimiao@live.cn>
2022-08-14 19:15:44 +0800

    refector: remove old file as tag v0.0.0 and start refector

---
+++ game/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: path.resolve('./src/'),
+    output: {
+        path: path.resolve('./dist'),
+        filename: 'index.js',
+        pathinfo: true
+    },
+    module: { // 静态资源引入图片 加上webpack url-loader解释器
+        rules: [{
+            test: /.(jpg|png)$/,
+            use: {
+              loader: 'url-loader',
+              options: {
+                limit: 1,
+              }
+            }
+        }],
+    },
+    plugins: [
+        new CopyWebpackPlugin({
+            patterns: [{
+                from: "./src/index.html", to: "./index.html"
+            }, {
+                from: "./src/assets", to: "./assets"
+            }]
+        })
+    ]
+};
